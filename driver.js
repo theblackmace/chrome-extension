72664a7fbdfdc7d237eb315314ac85a939be4d99
theblackmace <52634803+theblackmace@users.noreply.github.com>
2023-03-11 01:17:17 +0530

    commit-1

--- driver.js
+++ driver.js
@@ -1,5 +1,30 @@
-let inputBtn = document.querySelector("#input-btn")
+const inputBtn = document.querySelector("#input-btn")
+const inputEl = document.querySelector("#input-el")
+const ulEl = document.querySelector("#ul-el")
+
+let myLeads = []
 
 inputBtn.addEventListener("click", function(){
     console.log("Button clicked!")
+    myLeads.push(inputEl.value)
+    console.log(myLeads)
+    inputEl.value = ""
+    renderLeads()
 })
+
+// for loop logs out items in myLeads
+function renderLeads(){
+    let listItems = ""
+
+    for(let i=0; i<myLeads.length; i++){
+        listItems += `
+            <li>
+                <a target = "_blank" href = "${myLeads[i]}">
+                    ${myLeads[i]}
+                </a>
+            </li>
+        `
+    }
+    console.log(listItems)
+    ulEl.innerHTML = listItems
+}
