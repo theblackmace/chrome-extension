f7792bbe039f762ce5a91baedbd002f45135ea76
theblackmace <52634803+theblackmace@users.noreply.github.com>
2023-03-11 21:44:58 +0530

    update

--- driver.js
+++ driver.js
@@ -1,26 +1,57 @@
 const inputBtn = document.querySelector("#input-btn")
 const inputEl = document.querySelector("#input-el")
 const ulEl = document.querySelector("#ul-el")
+const deleteBtn = document.querySelector("#delete-btn")
+const tabBtn = document.querySelector("#tab-btn")
 
 let myLeads = []
+const tabs = [
+    {url: "https://www.linkedin.com/in/per-harald-borgen/"}
+]
+
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+console.log(leadsFromLocalStorage)
+
+if(leadsFromLocalStorage){
+    myLeads = leadsFromLocalStorage
+    render(myLeads)
+}
+
 
 inputBtn.addEventListener("click", function(){
-    console.log("Button clicked!")
+    console.log("Save Input Button clicked!")
     myLeads.push(inputEl.value)
-    console.log(myLeads)
     inputEl.value = ""
-    renderLeads()
+    localStorage.setItem("myLeads",JSON.stringify(myLeads))
+    console.log("Local Storage Array: "+localStorage.getItem("myLeads"))
+    render(myLeads)
 })
 
+tabBtn.addEventListener("click", function(){
+    console.log(tabs[0].url)
+    myLeads.push(tabs[0].url)
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    console.log("Local Storage Array: "+localStorage.getItem("myLeads"))
+    render(myLeads)
+})
+
+deleteBtn.addEventListener("dblclick", function(){
+    console.log("Delete Button clicked twice")
+    localStorage.clear()
+    myLeads = []
+    render(myLeads)
+})
+
+
 // for loop logs out items in myLeads
-function renderLeads(){
+function render(leads){
     let listItems = ""
 
-    for(let i=0; i<myLeads.length; i++){
+    for(let i=0; i<leads.length; i++){
         listItems += `
             <li>
-                <a target = "_blank" href = "${myLeads[i]}">
-                    ${myLeads[i]}
+                <a target = "_blank" href = "${leads[i]}">
+                    ${leads[i]}
                 </a>
             </li>
         `
